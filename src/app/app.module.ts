57a00cd70e0d31a5f5dad3395770eaa25500f7d7
themockingjester <yashmathur123123@gmail.com>
2023-03-08 02:05:35 +0530

    Added pages for login and register

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router'
 
+import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginPageComponent } from './Pages/login-page/login-page.component';
+import { SignUpPageComponent } from './Pages/sign-up-page/sign-up-page.component';
 
+const appRoutes: Routes = [
+  { path: "login", component: LoginPageComponent },
+  { path: "signup", component: SignUpPageComponent }
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPageComponent,
+    SignUpPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
+
   ],
   providers: [],
   bootstrap: [AppComponent]
